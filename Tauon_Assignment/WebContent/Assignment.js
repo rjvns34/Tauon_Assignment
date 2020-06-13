48cd60aa4deda8ddb263a25ce78f5370fe3aafab
rjvns34 <61065464+rjvns34@users.noreply.github.com>
2020-06-13 11:54:04 +0530

    Add files via upload

---
+++ Tauon_Assignment/WebContent/Assignment.js
@@ -0,0 +1,21 @@
+$.ajax({
+		url : 'https://jsonplaceholder.typicode.com/albums/1/photos',
+		type: 'GET',
+		data : {}, 
+		success : function(response)
+	 	{
+			var count = 0;
+			var data = response;
+			
+			$.each(data, function(i,value){
+				
+  				/* $("#tblData").append("<tr><td>"+value.albumId+"</td><td>"+value.id+"</td><td>"+value.title+"</td><td>"+value.url+"</td><td>"+value.thumbnailUrl+"</td></tr>"); */
+ 				$("#divData").append("<div class='col-lg-3'><div class='thumbnail'><a href='#''> <img class='img-thumbnail'src='"+value.url+"' title='"+value.title+"'><br></a><span class='pname'>"+value.title+"</span></div></div>");
+ 				count = count + 1;
+ 	 		});
+ 			$("#divHead").append("<div class='col-md-10'><h4 style='margin-top:30px'><span> "+count+" </span>Result(s) found for 'mask'</h4></div><div class='col-md-2'></div>");
+		 		
+			
+		}
+	});
+	
